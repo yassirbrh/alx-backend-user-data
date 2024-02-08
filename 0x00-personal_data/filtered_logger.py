16cbe96e1515a0ac91865d5a4026568d06ff12e9
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-08 20:07:21 +0000

    Function filter_datum that returns the log message obfuscated.

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+    Function filter_datum that returns the log message obfuscated.
+'''
+import re
+from typing import List
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str
+        ) -> str:
+    '''
+        filter_datum: function
+        @fields: List of strings representing all fields to obfuscate
+        @redaction: String representing by what the field will be obfuscated
+        @message: String representing the log line.
+        @separator: String representing by which character is separating
+                    all fields in the log line (message).
+        return: The log message obfuscated.
+    '''
+    output = message[0:]
+    for part in message.split(separator):
+        if part.split("=")[0] in fields:
+            output = re.sub(part.split("=")[1], redaction, output)
+    return output
