e4aed5a0af8004e941fa7ce8bd29ddd1614cba4a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-09 00:59:42 +0000

    Adding the format instance method to the class RedactingFormatter.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''
     Function filter_datum that returns the log message obfuscated.
 '''
+import logging
 import re
 from typing import List
 
@@ -25,3 +26,26 @@ def filter_datum(
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+            format: instance method.
+            @self: class constructor.
+            @record: LogRecord object.
+            return: The log message.
+        '''
+        msg = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
