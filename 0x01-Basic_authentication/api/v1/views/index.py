19b3084acea6aa98d6d9349dfe24a750f2693326
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-13 00:02:38 +0000

    Implementing an api route to return HTTP status code for a request unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,16 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """
+        unauthorized: function
+        description: raises an 401 error using abort.
+        return: None
+    """
+    abort(401)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
