c1c2b3f08a2b6d70dbe15a0bd68c196dfc4872da
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-13 19:41:40 +0000

    Implementing the class method extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,10 +3,24 @@
     Module to manage the Basic authentication
 '''
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     '''
         Class BasicAuth handles the Basic authentication.
     '''
-    pass
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''
+            extract_base64_authorization_header: instance method.
+            @self: class instance.
+            @authorization_header: Authorization Header.
+            return: Base64 header.
+        '''
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return re.search(r'Basic (.*)', authorization_header)
