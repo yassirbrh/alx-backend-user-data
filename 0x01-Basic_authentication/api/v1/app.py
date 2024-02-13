4c646dd4294effb6fe8f4aef02cc259b791f4d9b
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-13 18:52:01 +0000

    Class BasicAuth that inherits from Auth.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -18,6 +19,8 @@ auth = None
 
 if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+if getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+    Module to manage the Basic authentication
+'''
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    '''
+        Class BasicAuth handles the Basic authentication.
+    '''
+    pass
