bac69859167c6982a9f93e178e894e61213fd84b
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-13 15:56:50 +0000

    Updating the method def authorization_header(self, request=None) -> str, and the file api/v1/app.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -35,7 +35,7 @@ class Auth:
         '''
         if request is None:
             return None
-        return request.args.get('Authorization')
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''
