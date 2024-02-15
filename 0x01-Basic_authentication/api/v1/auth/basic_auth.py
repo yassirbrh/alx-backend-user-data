89b5229af6d347a7191848b5ce67a08549d38b01
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-15 00:04:45 +0000

    Implementing the instance method user_object_from_credentiels.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -77,6 +77,6 @@ class BasicAuth(Auth):
         if user_pwd is None or type(user_pwd) != str:
             return None
         user = User.search({'email': user_email})
-        if not user or user.is_valid_password(user_pwd):
+        if not user or User.is_valid_password(user_pwd):
             return None
         return user
