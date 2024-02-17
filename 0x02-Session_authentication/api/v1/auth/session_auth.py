b2ebc5ce45c395507fef572eb2306331048c09b0
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-17 23:09:39 +0000

    Creating the class attribute user_id_by_session_id and the instance method create_session to create sessions.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -21,7 +21,7 @@ class SessionAuth(Auth):
         '''
         if user_id is None or type(user_id) != str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
