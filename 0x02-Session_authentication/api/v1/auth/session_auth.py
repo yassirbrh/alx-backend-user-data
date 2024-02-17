2406d6682940521344556ec73d41288be691587a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-17 22:40:21 +0000

    Creating the class attribute user_id_by_session_id and the instance method create_session to create sessions.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,15 +3,24 @@
     Module to manage the Session authentication
 '''
 from .auth import Auth
-import base64
-import binascii
-from models.user import User
-import re
-from typing import TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
     '''
         Class SessionAuth handles the Session authentication.
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+            create_session: instance method.
+            @self: Class instance.
+            @user_id: User ID.
+            return: The Session ID.
+        '''
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = uuid.uuid4()
+        user_id_by_session_id[session_id] = user_id
+        return session_id
