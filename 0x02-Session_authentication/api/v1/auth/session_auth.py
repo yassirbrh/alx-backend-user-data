7d17dd2da11a88ddd56ced92a912efbcda27caff
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-17 23:41:42 +0000

    Adding the instance method current_user for the Class SessionAuth.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 '''
 from .auth import Auth
 import uuid
+from models import User
 
 
 class SessionAuth(Auth):
@@ -35,3 +36,16 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''
+            current_user: Instance method.
+            @self: Class Instance.
+            @request: Flask request object.
+            return: The current user.
+        '''
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
