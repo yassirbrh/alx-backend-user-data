d155fcd50cb31bf3465e96f6ebed3088015192cd
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-21 22:40:06 +0000

    Implementing the POST /users route that returns a JSON Payload.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,10 +2,12 @@
 '''
     The app module.
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -15,6 +17,21 @@ def main_page() -> dict:
         return: JSON payload.
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users() -> dict:
+    '''
+        users: function
+        return: JSON payload.
+    '''
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
