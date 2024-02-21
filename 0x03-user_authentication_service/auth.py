2ed310c8296d4886755bc514c27da834230e6ab2
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-21 20:19:50 +0000

    Implementing the Auth class method register_user.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
     Module for handling authentication system.
 '''
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -12,3 +14,24 @@ def _hash_password(password: str) -> bytes:
         return: the hashed password.
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(email: str, password: str) -> User:
+        '''
+            register_user: instance method
+            @email: Email.
+            @password: Password.
+            return: User Object.
+        '''
+        hashed = _hash_password(password)
+        try:
+            return self._db.add_user(email, password)
+        except ValueError:
+            print("User {} already exists.".format(email))
