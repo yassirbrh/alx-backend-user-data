c784023baf0864124c30f60b5d0b9ad0c60b9232
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-22 13:17:25 +0000

    Implementing the Auth instance method get_reset_password_token to update the reset_token.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -106,3 +106,18 @@ class Auth:
             pass
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''
+            get_reset_password_token: Instance method.
+            @self: Class instance.
+            @email: Email to check.
+            return: The reset token.
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
