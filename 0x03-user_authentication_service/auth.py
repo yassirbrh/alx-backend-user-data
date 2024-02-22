359b44bb80d5e608ecf3747c752ad4a4abc9034a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-22 09:21:13 +0000

    Implementing the instance method get_user_from_session_id that looks for a user object that had the session_id passed as argument.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -77,3 +77,18 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        '''
+            get_user_from_session_id: Instance method.
+            @self: Class instance.
+            @session_id: Session ID.
+            return: The User Object corresponding to the session id.
+        '''
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
