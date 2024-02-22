71837106e3ff18ff8108cc1a9d438374e9257767
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-22 15:40:17 +0000

    Implementing the route POST /reset_password to reset the user's password.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,5 +77,19 @@ def profile() -> dict:
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=['POST'])
+def get_reset_password_token() -> dict:
+    '''
+        get_reset_password_token: function.
+        return: JSON Payload.
+    '''
+    email = request.form.get("email")
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
