157080e3936c630f2cc0c52eec1a7a465790538d
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-22 15:52:17 +0000

    Implementing the Auth Class instance method update_password to update user's password.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -135,4 +135,8 @@ class Auth:
         except NoResultFound:
             raise ValueError()
         new_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=new_password)
+        self._db.update_user(
+                user.id,
+                hashed_password=new_password,
+                reset_token=None
+                )
